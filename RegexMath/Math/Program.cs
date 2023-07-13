e3abce7df132bec432801f8cfe5b206128595f9e
blvu <013ayu@gmail.com>
2023-07-13 20:55:34 +0800

    Added title, credits, exit ey
    new: Ctrl+x to exit loop
    new: member credits at exit

--- RegexMath/Math/Program.cs
+++ RegexMath/Math/Program.cs
@@ -6,49 +6,32 @@ namespace Math
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("====== Main Menu ======");
-            Console.WriteLine("1. Option 1");
-            Console.WriteLine("2. Option 2");
-            Console.WriteLine("3. Exit");
-
-            bool isExit = false;
-            do
+            while (true)
             {
-                string userOption = Console.ReadLine();
-                switch (userOption)
+                Console.WriteLine("==== Regex Math (By: Logic Gates ====)");
+                Console.WriteLine("--------------------------------------");
+                string input = Console.ReadLine();
+                bool success = RegexMath.RegexMath.TryEvaluate(input, out double result);
+                Console.WriteLine(success);
+                if (success)
+                    Console.WriteLine($"{input} = {result}");
+                
+                Console.WriteLine(" < Press any key to continue > ");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Modifiers == ConsoleModifiers.Control && keyInfo.Key == ConsoleKey.X)
                 {
-                    case "1":
-                        Console.WriteLine("Addition an Subtraction\n");
-                        Console.WriteLine("You have selected Option 1. Now, provide your implementation.\n");
-                        string input = Console.ReadLine();
-                        bool success = RegexMath.RegexMath.TryEvaluate(input, out double result);
-                        Console.WriteLine(success);
-                        if (success)
-                            Console.WriteLine($"{input} = {result}");
-                        break;
-                    case "2":
-                        Console.WriteLine("You have selected Option 2. Now, provide your implementation.");
-                        // Your code for option 2 goes here
-                        break;
-                    case "3":
-                        Console.WriteLine("============================================================================================");
-                        Console.WriteLine("      T H A N K   Y O U   F O R   U S I N G   S T U D E N T   I N F O R M A T I O N !");
-                        Console.WriteLine("============================================================================================");
-                        Console.WriteLine("\n\t\t\tFinal Project in Automata Theory and Formal Languages");
-                        Console.WriteLine("\t\t\t    Group Name: L O G I C   G A T E S\n");
-                        Console.WriteLine("\n\t\t\tCreated by: ");
-                        Console.WriteLine("\n\t\t\tManarang, Geryko Luis M. ");
-                        Console.WriteLine("\n\t\t\tBabasa, Victoria Faye M.");
-                        Console.WriteLine("\n\t\t\tCamasura, Jasmine M. ");
-                        Console.WriteLine("\n\t\t\tMurayama, Vincent Karl M. ");
-                        Console.WriteLine("\n\t\t\tOrdoñez, Kendric M. ");
-                        Console.WriteLine("============================================================================");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option, please choose option from 1, 2 or 3");
-                        break;
+                    break;
                 }
-            } while (!isExit);
+                Console.Clear();
+            }
+
+            Console.WriteLine("Regex Math (By: Logic Gates");
+            Console.WriteLine("Members: ");
+            Console.WriteLine("Geryko Manarang");
+            Console.WriteLine("Jasmine  Camasura");
+            Console.WriteLine("Kendric Ordonez");
+            Console.WriteLine("Victoria Babasa");
         }
     }
 }
